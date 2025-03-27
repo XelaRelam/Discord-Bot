18bb908e7482bd9c2d13f1c902c234036b3ec826
Dark-LYNN <botdevlynn@gmailcom>
2025-03-27 19:40:34 +0100

    feat: function to see if user exists.

---
+++ src/middleware/userExists.ts
@@ -0,0 +1,14 @@
+import { Client } from "discord.js";
+import { userIdRegex } from "../types/regex";
+
+export const userExists = async (client: Client, userID: string): Promise<boolean> => {
+  if (!userIdRegex.test(userID)) return false;
+
+  try {
+    await client.users.fetch(userID, { force: true });
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
