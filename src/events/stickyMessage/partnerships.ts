1a8373b4405591a0a970116968f4553d345ccc92
Dark-LYNN <botdevlynn@gmailcom>
2025-05-08 20:00:37 +0200

    More stickynotes

---
+++ src/events/stickyMessage/partnerships.ts
@@ -0,0 +1,68 @@
+import { ExtendedClient } from '../../types/extendedClient';
+import { Message, TextChannel } from 'discord.js';
+import { prisma } from '../../database';
+
+const STICKY_CHANNELS = new Set(['1266455854811578379']);
+
+export const searchScript = async (
+  client: ExtendedClient,
+  message: Message,
+):Promise<void> => {
+  if ((client.env('ENVIRONMENT')) === 'dev') return;
+  if (
+    message.author.bot && message.author.id !== client.user?.id
+  ) { // handle sticky If message is sent (not the bot itself)
+    if (!STICKY_CHANNELS.has(message.channel.id)) return;
+    const channel = message.channel as TextChannel;
+    try {
+      const lastSticky = await prisma.stickyMessage.findFirst({
+        where: { channelID: channel.id },
+      });
+
+      /**
+       * @description Deletes the last sticky Message
+       */
+      if (lastSticky) {
+        try {
+          const lastMessage =
+            await channel.messages.fetch(lastSticky.messageID);
+          if (lastMessage && lastMessage.author.id === client.user?.id) {
+            await lastMessage.delete();
+          }
+        } catch (err) {
+          console.warn(`⚠️ | Could not delete previous sticky message: ${err}`);
+        }
+      }
+
+      /**
+       * @description Send new message
+       */
+      const botMessage = await channel.send({
+        content: '**Want to partner with us ?**\n\n' +
+          '> Check out our https://discord.com/channels/1235165357879328870/1235205893508042903/1237756993620410420 \n\n' +
+          '> If you think you meet requirements, straight go to <#1235241560023502889>.\n\n'
+      });
+
+      /**
+       * @description Update database to have latest sticky message
+       */
+      if (lastSticky) {
+        await prisma.stickyMessage.update({
+          where: { id: lastSticky.id },
+          data: { messageID: botMessage.id },
+        });
+      } else {
+        await prisma.stickyMessage.create({
+          data: {
+            channelID: channel.id,
+            messageID: botMessage.id,
+          },
+        });
+      }
+      return;
+    } catch (err) {
+      console.error(`❌ | Failed to handle sticky message: ${err}`);
+      return;
+    }
+  }
+};
